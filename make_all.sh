855c68372ed22b80edc8d17f6e7b576454268fbb
Martin Grap <rmsk2@gmx.de>
2022-04-20 09:23:47 +0200

    Add script to build all variants

---
+++ make_all.sh
@@ -0,0 +1,20 @@
+rm cbmcrypt_c64.d64
+rm cbmcrypt_vic20.d64
+rm cbmcrypt_plus4.d64
+rm cbmcrypt_c128.d64
+
+make clean
+make 
+cp cbmcrypt.d64 cbmcrypt_c64.d64
+
+make clean
+make VIC20=1
+cp cbmcrypt.d64 cbmcrypt_vic20.d64
+
+make clean
+make PLUS4=1
+cp cbmcrypt.d64 cbmcrypt_plus4.d64
+
+make clean
+make C128=1
+cp cbmcrypt.d64 cbmcrypt_c128.d64
